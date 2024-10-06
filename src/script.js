d2889c463e57f6b8a5f06d5ca6d36d5b99dab5ad
EugeneWu11 <genewu013@gmail.com>
2024-10-06 17:13:13 -0600

    P#2: Se agrega slider de proyectos a la segunda sección del portafolio

---
+++ src/script.js
@@ -0,0 +1,33 @@
+//Script del slider
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+    //Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+
+    breakpoints: {
+        //Celulares
+        0:{
+            slidesPerView:1
+        },
+        //Tablets
+        768:{
+            slidesPerView:2
+        },
+        //Pc
+        1024:{
+            slidesPerView:3
+        },
+    }
+  });
